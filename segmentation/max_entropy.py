4483c45f0bb64a78bb50115a2270308d195f9640
Robert Metchev <rmetchev@hotmail.com>
2016-04-29 09:27:35 -0700

    Initial commit

---
+++ segmentation/max_entropy.py
@@ -0,0 +1,67 @@
+import numpy as np
+"""
+Implements Kapur-Sahoo-Wong (Maximum Entropy) thresholding method
+Usage: $ python max_entropy.py <gray scale image>
+"""
+
+
+def max_entropy(data):
+    """
+    Implements Kapur-Sahoo-Wong (Maximum Entropy) thresholding method
+    Kapur J.N., Sahoo P.K., and Wong A.K.C. (1985) "A New Method for Gray-Level Picture Thresholding Using the Entropy
+    of the Histogram", Graphical Models and Image Processing, 29(3): 273-285
+    M. Emre Celebi
+    06.15.2007
+    Ported to ImageJ plugin by G.Landini from E Celebi's fourier_0.8 routines
+
+    2016-04-28: Adapted for Python 2.7 by Robert Metchev from Java source of MaxEntropy() in the Autothresholder plugin
+    http://rsb.info.nih.gov/ij/plugins/download/AutoThresholder.java
+
+    :param data: Sequence representing the histogram of the image
+    :return threshold: Resulting maximum entropy threshold
+    """
+
+    # calculate CDF (cumulative density function)
+    cdf = data.astype(np.float).cumsum()
+
+    # find histogram's nonzero area
+    valid_idx = np.nonzero(data)
+    first_bin = np.min(valid_idx)
+    last_bin = np.max(valid_idx)
+
+    # initialize search for maximum
+    max_ent, threshold = 0, 0
+
+    for it in range(first_bin, last_bin + 1):
+        # Background (dark)
+        hist_range = data[:it + 1] / cdf[it]  # normalize witin selected range
+        hist_range = hist_range[hist_range != 0]  # remove all zero elements
+        tot_ent = -np.sum(hist_range * np.log(hist_range))  # background entropy
+
+        # Foreground/Object (bright)
+        hist_range = data[it + 1:] / (cdf[last_bin] - cdf[it])  # normalize witin selected range
+        hist_range = hist_range[hist_range != 0]  # remove all zero elements
+        tot_ent -= np.sum(hist_range * np.log(hist_range))  # accumulate object entropy
+
+        # find max
+        if tot_ent > max_ent:
+            max_ent, threshold = tot_ent, it
+
+    return threshold
+
+
+# test
+if __name__ == '__main__':
+    import sys
+    import skimage.io
+
+    # read default image used during development - threshold should be 67, or get image file from command line
+    img_name = 'Rat_Hippocampal_Neuron.png'
+    if len(sys.argv) > 1:
+        img_name = sys.argv[1]
+    img = skimage.io.imread(img_name)
+    # obtain histogram
+    hist = np.histogram(img, bins=256, range=(0, 256))[0]
+    # get threshold
+    th = max_entropy(hist)
+    print th
